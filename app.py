aec8408dde7f6cb0e4965ea1a3912d5304f64c7d
Scott Pierce <ddrscott@gmail.com>
2024-05-08 10:37:06 -0500

    Oops

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ async def on_message(message):
 
     if f'<@{bot.user.id}>' in message.content:
         pass
-    if f'@{bot.user.name}' in message.content:
+    elif f'@{bot.user.name}' in message.content:
         pass
     elif message.channel.type == discord.ChannelType.private:
         pass
